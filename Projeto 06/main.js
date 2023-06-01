7fc1d299f1c66c66cb028549d46e197b517489ef
Xan Calavera <xancalavera@gmail.com>
2023-06-01 19:31:17 -0300

    update

--- Projeto 06/main.js
+++ Projeto 06/main.js
@@ -5,13 +5,14 @@ const screen2 = document.querySelector('.screen2');
 const btnTry = document.querySelector("#btnTry");
 const btnReset = document.querySelector("#btnReset");
 
-const randomNumber = Math.round(Math.random() * 10);
+let randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
 
 
 // Eventos
 btnTry.addEventListener('click', handleTryClick);
 btnReset.addEventListener('click', handleResetClick);
+document.addEventListener('keydown', enterReset)
 
 
 // Função callback
@@ -19,6 +20,7 @@ function handleTryClick(event) {
   event.preventDefault() //não faça o padrão desse evento, que é um botão dentro de um formulário sería enviar o formulário
   
   const inputNumber = document.querySelector('#inputNumber')
+  
 
   if(Number(inputNumber.value) == randomNumber) {
     toggleScreen()
@@ -26,13 +28,30 @@ function handleTryClick(event) {
     screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas!`
   }
 
-  inputNumber.value = ""
+  else if (isNaN(Number(inputNumber.value))) {
+    alert('Se não é número não vale!')
+    xAttempts--
+  }
+
+  else if (Number(inputNumber.value) > 10 || Number(inputNumber.value) < 0)   {
+    alert('Insira um número entre 0 e 10')
+    xAttempts--
+  }
+
+  else if (Number(inputNumber.value) == "") {
+    alert("Por favor insira um número")
+    xAttempts--
+  }
+ 
   xAttempts++
+  inputNumber.value = ""
+  
 };
 
 function handleResetClick() {
   toggleScreen()
   xAttempts = 1
+  randomNumber = Math.round(Math.random() * 10);
 };
 
 
@@ -41,3 +60,8 @@ function toggleScreen() {
   screen2.classList.toggle('hide')
 };
 
+function enterReset (e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
+}
