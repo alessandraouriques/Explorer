5db4e309d00c05b58c4ddaae7375c348e5e8f876
Xan Calavera <xancalavera@gmail.com>
2023-06-08 21:31:24 -0300

    starting JS

---
+++ Projeto 08/script.js
@@ -0,0 +1,12 @@
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds');
+
+const btnPlay = document.querySelector('.play');
+const btnPause = document.querySelector('.pause');
+const btnStop = document.querySelector('.stop');
+const btnSoundOn = document.querySelector('.sound-on');
+const btnSoundOff = document.querySelector('.sound-off');
+
+
+
+
